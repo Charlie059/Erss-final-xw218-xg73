a10a8e6e290121a3a8554d4fc8a0fcce18e18a89
xw218 <xw218@duke.edu>
2022-04-17 12:21:14 -0400

    init amazon ups connection

--- backend/app/src/main/java/edu/duke/ece568/App.java
+++ backend/app/src/main/java/edu/duke/ece568/App.java
@@ -3,15 +3,51 @@
  */
 package edu.duke.ece568;
 
+import edu.duke.ece568.utils.AmazonConnector;
 import edu.duke.ece568.utils.WorldConnect;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    final String WORLD_HOST = "localhost";
+    final int WORLD_PORT = 23456;
+    private static long seqnum=0;
+    private WorldConnect worldConnector;
+    private AmazonConnector amazonConnector;
 
-        final String WORLD_HOST = "207.246.90.49";
-        final int WORLD_PORT = 23456;
+    public App(){//TODO pass hosts and ports info
 
-        WorldConnect worldConnector = new WorldConnect(WORLD_HOST, WORLD_PORT);
+    }
+
+    /**
+     * Connects to world/Connects to amazon
+     * @throws IOException
+     */
+    public void setup() throws IOException {
+        //TODO refactor to multiple thread
+        worldConnector = new WorldConnect(WORLD_HOST, WORLD_PORT);
         worldConnector.setupConnection();
+
+        //amazonConnector = new AmazonConnector(worldConnector.getWorldid());
+        //amazonConnector.connectAmazon_socket();
+
+    }
+
+    public void run(){
+        //Run Amazon Listener/World Listener/Front end Listener
+    }
+
+    /**
+     * Gets and increment the seqnum
+     * @return the requested sequence number
+     */
+    public static synchronized long getSeqnum(){
+        return seqnum++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        app.setup();
+        app.run();
     }
 }
---
+++ backend/app/src/main/java/edu/duke/ece568/utils/AmazonConnector.java
@@ -0,0 +1,59 @@
+package edu.duke.ece568.utils;
+
+import edu.duke.ece568.App;
+import edu.duke.ece568.proto.UpsAmazon;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static edu.duke.ece568.utils.GPBHelper.recvMsgFrom;
+import static edu.duke.ece568.utils.GPBHelper.sendMsgTo;
+
+public class AmazonConnector {
+    private int port;//used for connect to amazon
+    private ServerSocket serverSocket;
+    private InputStream in;
+    private OutputStream out;
+    private long worldid;
+    private Socket amazon_socket;
+
+    public AmazonConnector(int port, long worldid) throws IOException {
+        this.port = port;
+        this.worldid = worldid;
+        serverSocket = new ServerSocket(port);
+    }
+
+    /**
+     * Waits fro Amazon socket connection
+     * @throws IOException
+     */
+    public void connectAmazon_socket() throws IOException {
+        System.out.println("Waiting for connection from Amazon");
+        amazon_socket = serverSocket.accept();
+        this.out = amazon_socket.getOutputStream();
+        this.in = amazon_socket.getInputStream();
+    }
+
+    public UpsAmazon.USendWorldID init_USendWorldID(){
+        UpsAmazon.USendWorldID.Builder new_world_builder = UpsAmazon.USendWorldID.newBuilder();
+        new_world_builder.setSeqnum(App.getSeqnum());
+        new_world_builder.setWorldId(worldid);
+        return new_world_builder.build();
+    }
+
+    /**
+     * Processes the world transimission between Amazon and Ups
+     * Generates USendWorldID
+     */
+    public long processWorldMsg(){
+        UpsAmazon.USendWorldID uSendWorldID = init_USendWorldID();
+        sendMsgTo(uSendWorldID, out);
+        System.out.println("Sending ups connect message to Amazon");
+        UpsAmazon.AUResponse.Builder auResponse = UpsAmazon.AUResponse.newBuilder();
+        recvMsgFrom(auResponse, in);
+        System.out.println("Receiving ack from Amazon: " + auResponse.getAcks(0));
+        return auResponse.getAcks(0);
+    }
+}
